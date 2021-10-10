a3569cb523022613a4cff24da94cc2b8c3111465
bond8128 <90555849+bond8128@users.noreply.github.com>
2021-10-10 16:44:44 +0700

    Create upload2.sh

---
+++ upload2.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+
+upload_proxy() {
+    cd $WORKDIR
+    local PASS=$(random)
+    zip --password $PASS proxy.zip proxy.txt
+    URL=$(curl -F "file=@proxy.zip" https://file.io)
+
+    echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
+    echo "Download zip archive from: ${URL}"
+    echo "Password: ${PASS}"
+
+}
+
+
+
+bash /etc/rc.local
+
+gen_proxy_file_for_user
+
+upload_proxy
