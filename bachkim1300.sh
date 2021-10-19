7e5d197fce139db5ba7f8b5f473f5378e7fe88f4
bond8128 <90555849+bond8128@users.noreply.github.com>
2021-10-19 12:40:49 +0700

    Update bachkim1300.sh

--- bachkim1300.sh
+++ bachkim1300.sh
@@ -114,8 +114,8 @@ IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
-FIRST_PORT=40001
-LAST_PORT=41300
+FIRST_PORT=50001
+LAST_PORT=51300
 
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
